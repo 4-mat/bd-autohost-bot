9f0eba56f6b56ee8f0a541322eb5768365380eef
4-mat <36202270+4-mat@users.noreply.github.com>
2023-03-17 14:52:38 -0400

    updated retrieve.js
    added code that removes the abilities greater than the user's current level from the abilities array returned by the function

--- retrieve.js
+++ retrieve.js
@@ -7,6 +7,7 @@ function toID(str){
 function getWeaponAbilities(weapon, level){
     let data = require('origindb')('data');
     let weaponIndex = data('branch').get('weapon');
+    console.log(weaponIndex);
 
     // find if the weapon is inside the branch variable 
     if(weaponIndex.indexOf(weapon) > -1){
@@ -16,7 +17,10 @@ function getWeaponAbilities(weapon, level){
             if (data('branch').get(branchIndex[i]).includes(weapon)){
                 // get the weapon's abilities
                 let abilities = data(toID(branchIndex[i])).get(weapon);
+                // remove the abilities greater than the user's current level
+                abilities = abilities.filter(ability => ability.Lvl <= level);
                 console.log(abilities);
+                return abilities;
             }
         }
     }
@@ -27,3 +31,5 @@ module.exports =  {
         getWeaponAbilities(weapon, level);
     }
 }
+
+getWeaponAbilities("Spellbook", 8);
